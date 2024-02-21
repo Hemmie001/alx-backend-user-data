7ed38700f06d37debb768c64070ce2d7296eea2c
Hemmie001 <emmanuelakej@gmail.com>
2024-02-21 15:36:28 +0000

    5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -59,15 +59,17 @@ def authenticate_user():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request) is None:
+    auth_header = auth.authorization_header(request)
+    session_cookie = auth.session_cookie(request)
+    if auth_header is None and session_cookie is None:
         abort(401)
-
-    current_user = auth.current_user(request)
-    if current_user is None:
+    # If auth.current_user(request) returns None, raise the error 403 - you
+    # must use abort
+    user = auth.current_user(request)
+    if user is None:
         abort(403)
-
-    request.current_user = current_user
+    # Assign the result of auth.current_user(request) to request.current_user
+    request.current_user = user
 
 
 if __name__ == "__main__":
