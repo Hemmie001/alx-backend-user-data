5d908415ee727747557722be06f08707053a09e5
Hemmie001 <emmanuelakej@gmail.com>
2024-02-23 14:49:37 +0000

    0

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -2,7 +2,6 @@
 """Module for authentication"""
 from typing import List, TypeVar
 from flask import request
-from os import getenv
 
 
 class Auth():
@@ -75,18 +74,3 @@ class Auth():
         For now, it simply returns None.
         """
         return None
-
-    def session_cookie(self, request=None):
-        """Returns a cookie value from a request"""
-
-        if request is None:
-            return None
-
-        SESSION_NAME = getenv("SESSION_NAME")
-
-        if SESSION_NAME is None:
-            return None
-
-        _my_session_id = request.cookies.get(SESSION_NAME)
-
-        return _my_session_id
