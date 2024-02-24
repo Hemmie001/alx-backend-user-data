dc8cb2da95c0ca2a938770001760655a730a3c72
Hemmie001 <emmanuelakej@gmail.com>
2024-02-24 00:03:11 +0000

    1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -16,7 +16,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':    
+if auth_type == 'auth':
     auth = Auth()
 elif auth_type == 'basic_auth':
     auth = BasicAuth()
