94767899aba8eb55473a7006266d90c77aac0679
Hemmie001 <emmanuelakej@gmail.com>
2024-02-27 14:23:17 +0000

    .

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -10,18 +10,24 @@ from models.user import User
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
+    """
+    Handles user login/session creation &gets email/password from request form
+    """
     email = request.form.get('email')
     password = request.form.get('password')
 
+    # Validate email and password
     if not email:
         return jsonify({"error": "email missing"}), 400
     if not password:
         return jsonify({"error": "password missing"}), 400
 
+    # Find user by email
     user = User.search({'email': email})
     if not user:
         return jsonify({"error": "no user found for this email"}), 404
 
+    # Check password
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
 
