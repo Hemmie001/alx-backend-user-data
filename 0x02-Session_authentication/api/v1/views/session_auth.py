7efe9cfb2d581b15e6e1ea8d31c7954ee1f547a2
Hemmie001 <emmanuelakej@gmail.com>
2024-02-27 14:32:54 +0000

    7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -5,7 +5,9 @@ authentication routes
 """
 
 from flask import request, jsonify, abort
-from api.v1.app import auth
+from api.v1.app import app
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
 from models.user import User
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
