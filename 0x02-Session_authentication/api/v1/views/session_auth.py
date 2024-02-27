96319f9d01c32d65170724b049b10424f8878219
Hemmie001 <emmanuelakej@gmail.com>
2024-02-27 14:10:25 +0000

    7

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,26 @@
+from flask import request, jsonify, abort
+from api.v1.app import auth
+from models.user import User
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({'email': email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    response.set_cookie(auth.SESSION_NAME, session_id)
+
+    return response
