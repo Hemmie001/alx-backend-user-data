d17d42adcca7131520b317403817f537d1f184b1
Hemmie001 <emmanuelakej@gmail.com>
2024-02-27 21:17:33 +0000

    0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,8 +55,7 @@ def authenticate_user():
             '/api/v1/auth_session/login/',
         ]
         if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
-            session_cookie = auth.session_cookie(request)
+            auth_header = auth.authorization_header(request)            
             if auth_header is None and session_cookie is None:
                 abort(401)
             user = auth.current_user(request)
